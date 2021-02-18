9c43d1686449c27e6959b10573c954486240fd03
FlutLab.io <>
2021-02-18 14:12:29 +0000

    Autosave

--- lib/screens/SplashScreen/splashScreen.dart
+++ lib/screens/SplashScreen/splashScreen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class MyApp extends StatelessWidget {
+class Splashscreen extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
